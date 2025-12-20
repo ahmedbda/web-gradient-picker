dffa6ea5d463e6cea480e5fdbe4fd8cd6425b6df
Ahmed <120943404+ahmedbda@users.noreply.github.com>
2025-12-20 19:48:49 +0100

    br for source code

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,8 @@ function App() {
               Click on the buttons above this one to<br />change the page's background gradient!
               <div className="absolute w-2 h-2 bg-gray-800/90 backdrop-blur-sm transform rotate-45 top-[-4px] left-1/2 -translate-x-1/2"></div>
             </div>
-        )}
+        )}<br>
+           
         <a href="https://github.com/ahmedbda/web-gradient-picker">Source code</a>
         
         </div>
